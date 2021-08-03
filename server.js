87eae4e86e49067da3bd72eeeb28b41f037c811e
abdelqader-alomari <aboud.coding@gmail.com>
2021-08-03 18:42:37 +0300

    updated

--- server.js
+++ server.js
@@ -29,12 +29,12 @@ server.get('/weather', (req, res) => {
             element.city_name.toLowerCase() === searchQuery.toLowerCase() ||
             (element.lat === lat && element.lon === lon) // to search by city or (lat+lon)
         );
-
+        if (!cityInfo) res.status(404).send('There is no data to show for this destination');
         let forecastArr = cityInfo.data.map(info => new Forecast(info));
         console.log(forecastArr);
         res.send(forecastArr);
     }
-    catch { res.status(404).send('There is no data to show for this destination'); }
+    catch { }
 });
 
 server.listen(PORT, () => console.log(`listening on ${PORT}`));
