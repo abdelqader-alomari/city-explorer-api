ed6f554e845948dba446785bd3536cae590ea53c
abdelqader-alomari <aboud.coding@gmail.com>
2021-08-05 13:59:18 +0300

    Memory added for movies API

--- movies.js
+++ movies.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const axios = require('axios');
-
+let moviesMemory = {};
 
 class Movie {
     constructor(movie) {
@@ -16,20 +16,28 @@ class Movie {
 }
 
 async function movies(req, res) {
-    let returnArr = [];
+    let moviesArr = [];
     let { cityName } = req.query;
-    const URL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${cityName}`;
-
-    try {
-        let axiosMovies = await axios.get(URL)
-        console.log(axiosMovies.data.results);
-        axiosMovies.data.results.map(item => returnArr.push(new Movie(item)));
-        res.status(200).send(returnArr);
+    if (moviesMemory[cityName] !== undefined) {
+        console.log('data from the memory')
+        res.send(moviesMemory[cityName]);
     }
-    catch (error) {
-        res.status(400)
-        if (error.status) res.status(error.status).send(error.message)
+    else {
+        const URL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${cityName}`;
+
+        try {
+            let axiosMovies = await axios.get(URL)
+            axiosMovies.data.results.map(item => moviesArr.push(new Movie(item)));
+            console.log('data from the API')
+            moviesMemory[cityName] = moviesArr;
+            res.status(200).send(moviesArr);
+        }
+        catch (error) {
+            res.status(400)
+            if (error.status) res.status(error.status).send(error.message)
+        }
     }
 }
+
 
 module.exports = movies
